23fd334d00152017e55df7b34fc19806959e1acf
visionary <0610mmmm>
2019-05-16 17:41:43 +0800

    project frame

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,26 @@
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
 module.exports = {
     devServer: {
+        overlay: {
+          warnings: true,
+          errors: true
+        },
+        open: IS_PROD,
+        host: '0.0.0.0',
+        port: 8000,
+        https: false,
+        hotOnly: false,
         proxy: {
             '/api': {
                 target: 'http://39.105.35.189:3000',
-                changeOrigin: true
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '/'
+                }
             }
         }
+        // proxy:'http://39.105.35.189:3000'
     }
-};
+}
+
+
